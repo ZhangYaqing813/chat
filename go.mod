488efc3d460282a417bef3e1870a17693ff1f49d
zhangyaqing <zhangyaqing59@126.com>
2021-12-02 18:32:36 +0800

    ""

--- go.mod
+++ go.mod
@@ -2,9 +2,9 @@ module chat
 
 go 1.17
 
-require github.com/cihub/seelog v0.0.0-20170130134532-f561c5e57575
-
 require (
-	github.com/go-sql-driver/mysql v1.6.0 // indirect
-	github.com/jmoiron/sqlx v1.3.4 // indirect
+	github.com/cihub/seelog v0.0.0-20170130134532-f561c5e57575
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/gomodule/redigo v1.8.5
+	github.com/jmoiron/sqlx v1.3.4
 )
--- server/dba/redis/redis.go
+++ server/dba/redis/redis.go
@@ -1,1 +1,52 @@
 package redism
+
+import (
+	"fmt"
+	"github.com/gomodule/redigo/redis"
+	"time"
+)
+
+var MyRedis *RedisOpt
+
+var RedisPool *redis.Pool
+
+type RedisOpt struct {
+	pool *redis.Pool
+}
+
+func RedisPools(address string, maxIdle, maxActive int, idleTimeout time.Duration) {
+	RedisPool = &redis.Pool{
+		MaxIdle:     maxIdle,
+		MaxActive:   maxActive,
+		IdleTimeout: idleTimeout,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", address)
+			if err != nil {
+				return nil, err
+			}
+			return c, err
+		},
+	}
+}
+
+func RedisFac(pool *redis.Pool) (MyRedis *RedisOpt) {
+	MyRedis = &RedisOpt{
+		pool: pool,
+	}
+	return
+}
+
+func (R *RedisOpt) Add(db, data string, username string) (code int, err error) {
+	//申请连接
+	conn := R.pool.Get()
+	defer func() {
+		err := conn.Close()
+		if err != nil {
+			fmt.Println("redis conn close failed ", err)
+		}
+	}()
+
+	//_, err := conn.Do("HSET","")
+
+	return
+}
