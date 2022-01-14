8899f5965d932f96d166567b26b09fc07d1d9939
zhangyaqing <zhangyaqing59@126.com>
2022-01-14 17:33:52 +0800

    "解析配置文件更新"

--- loadConfig/loadConfig.go
+++ loadConfig/loadConfig.go
@@ -18,7 +18,8 @@ func ServerFac(file string) *messagetype.ServerConfig {
 	return &ServerCfg
 }
 
-func ClientFac() *messagetype.ClientConfig {
+func ClientFac(file string) *messagetype.ClientConfig {
+	CCfg(file, &ClientCfg)
 	return &ClientCfg
 }
 
@@ -43,81 +44,146 @@ func ServerLoadConfig(file string, ServerCfg *messagetype.ServerConfig) {
 	cfg := strings.Split(string(b), "\r\n")
 	//2、解析
 	fmt.Println(cfg)
-
-	//3、对应字段赋值
-
-}
-
-func loadini(configFile string, cfg *config) {
-	conType := reflect.TypeOf(cfg)
-	conVaule := reflect.ValueOf(cfg)
-	var confStructName string
-	var sectionName string
-
-	b, err := ioutil.ReadFile(configFile)
-	if err != nil {
-		fmt.Println("read config file failed ", err)
-	}
-	// 读取配置文件，并且跟"\r\n" 换行进行字符串分割
-	conf := strings.Split(string(b), "\r\n")
-	//fmt.Println(conf)
-	// 循环处理读取的配置文件
-	for idx, line := range conf {
-		//判断是否符合相应的要求
-		// 空行，注释去掉
+	//2.1 解析读取到的配置文件
+	for idx, line := range cfg {
+		//2.2 判断配置文件中行是否存在不符合的语法的行
+		// 去掉空行，注释行，以及"["开头不是"]"结尾，"="开头的行进行提示、过滤
+		line = strings.TrimSpace(line)
 		if len(line) == 0 || strings.HasPrefix(line, ";") || strings.HasPrefix(line, "#") {
 			continue
 		}
-
-		//判断格式是否满足
-		if strings.HasPrefix(line, "=") || line[0] == '[' && line[len(line)-1] != ']' {
-			//提示哪一行不符合规范
-			err := fmt.Errorf("config file line %d is wrong ", idx+1)
-			fmt.Println(err)
+		if line[0] == '[' && line[len(line)-1] != ']' || line[0] == '=' {
+			chatlog.Std.Errorf("config.ini 第 %d 行格式错误", idx+1)
+			return
 		}
-		// 获取配置的节点信息,并保存到sectionName
-		if line[0] == '[' && line[len(line)-1] == ']' {
-
+		//2.3 找到对应的节点，保存。
+		// [] 表示节点的开始
+		if len(line) > 0 && line[0] == '[' && line[len(line)-1] == ']' {
 			sectionName = strings.Trim(line, "[]")
-			fmt.Println("找到配置文件 ", sectionName)
-			//遍历配置结构体，查询是否有对应的配置文件
+			chatlog.Std.Infof("config 找到节点%s 信息,并缓存至 sectionName 中", sectionName)
+			//2.4  解析代码中对应的结构体中的节点信息
+			//遍历结构体字段信息
 			for i := 0; i < conType.Elem().NumField(); i++ {
 				fieldName := conType.Elem().Field(i)
+				//根据代码中配置结构体的tag 字段信息找到对应的节点信息
 				if fieldName.Tag.Get("ini") == sectionName {
-					confStructName = fieldName.Name
-					fmt.Println("在结构体中查询到对应的配置信息", confStructName)
+					conStructName = fieldName.Name
+					chatlog.Std.Infof("找到与配置文件 %s 应的节点信息")
 					break
 				}
 			}
 
 		} else {
-			//切割行变换成 切片，条件“=”
-			line := strings.Split(line, "=")
-			// 获取kv
-			key := strings.TrimSpace(line[0])
-			vaule := strings.TrimSpace(line[1])
-			// 根据上文拿到的代码中的配置
-			subConV := conVaule.Elem().FieldByName(confStructName)
-			subConT := subConV.Type()
-			var subConFieldName string
-			var subConFieldType reflect.StructField
+			//3、对应字段赋值
+			//3.1 解析line 中的字段，获取key和value
+			confLine := strings.Split(line, "=")
+			//等号左边的是key
+			key := strings.TrimSpace(confLine[0])
+			//等号右边的是value
+			value := strings.TrimSpace(confLine[1])
 
-			for i := 0; i < subConV.NumField(); i++ {
-				field := subConT.Field(i)
-				subConFieldType = field
-				if field.Tag.Get("ini") == key {
-					subConFieldName = field.Name
-					fmt.Println(subConFieldName)
+			//3.2 获取代码中配置struct 中节点的信息
+			//获取sectionName 中的值信息
+			subConfV := conValue.Elem().FieldByName(conStructName)
+			// 获取subConfV 的类型信息
+			subConfT := subConfV.Type()
+
+			//3.2 遍历字段信息
+			if subConfT.Kind() != reflect.Struct {
+				chatlog.Std.Error("该配置节点不是不符合要求")
+			}
+			//缓存一下找到的每一个节点的字段信息
+			var subConFieldName string
+			var fieldName reflect.StructField
+			for i := 0; i < subConfT.NumField(); i++ {
+				fieldName = subConfT.Field(i)
+				//根据ini tag 进行对比，找到字段后跳出
+				if fieldName.Tag.Get("ini") == key {
+					//找到字段后保存字段名
+					subConFieldName = fieldName.Name
+					chatlog.Std.Infof("找到对应字段%s", key)
 					break
 				}
 			}
-			fieldObj := subConV.FieldByName(subConFieldName)
-			switch subConFieldType.Type.Kind() {
+			//3.3 对应的字段进行赋值操作
+			//获取到节点字段
+			fieldobj := subConfV.FieldByName(subConFieldName)
+			switch fieldobj.Type().Kind() {
 			case reflect.String:
-				fieldObj.SetString(vaule)
-			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-				v, _ := strconv.ParseInt(vaule, 10, 64)
-				fieldObj.SetInt(v)
+				fieldobj.SetString(value)
+			case reflect.Int, reflect.Int16, reflect.Int32, reflect.Int64:
+				v, err := strconv.ParseInt(value, 10, 64)
+				if err != nil {
+					chatlog.Std.Errorf("%s 类型转换失败", value)
+				}
+				fieldobj.SetInt(v)
+			}
+		}
+	}
+}
+
+// ClientCfg 配置文件解析
+
+func CCfg(file string, clientcfg *messagetype.ClientConfig) {
+	var sectionName string
+
+	confT := reflect.TypeOf(clientcfg)
+	confV := reflect.ValueOf(clientcfg)
+	// 1、加载配置文件
+	b, err := ioutil.ReadFile(file)
+	if err != nil {
+		chatlog.Std.Errorf("读取配置文件失败 %v", err)
+	}
+	clientCfg := strings.Split(string(b), "\r\n")
+
+	//2、解析配置文件
+	for index, line := range clientCfg {
+		//处理注释空行
+		if len(line) == 0 || strings.HasPrefix(line, ";") || strings.HasPrefix(line, "#") {
+			continue
+		}
+		//处理错误表达式
+		if line[0] == '[' && line[len(line)-1] != ']' || strings.HasPrefix(line, "=") {
+			chatlog.Std.Errorf("%s 中第 %d 语法错误", file, index+1)
+			return
+		}
+
+		// 查找节点信息
+		if line[0] == '[' && line[len(line)-1] == ']' && len(line) > 0 {
+			sectionName = strings.Trim(line, "[]")
+			chatlog.Std.Info("找到节点信息", line)
+			// 判断代码中传递的是否为结构体，
+			if confV.Elem().String() == sectionName && confT.Elem().Kind() == reflect.Ptr && confV.Elem().Kind() == reflect.Struct {
+				chatlog.Std.Info("配置文件对应")
+			}
+		} else {
+			lcf := strings.Split(line, "=")
+			key := strings.TrimSpace(lcf[0])
+			value := strings.TrimSpace(lcf[1])
+			var field reflect.StructField
+			var fieldName string
+			// 遍历结构体字段信息
+
+			for i := 0; i < confV.Elem().NumField(); i++ {
+				field = confT.Field(i)
+				if field.Tag.Get("ini") == key {
+					fieldName = field.Name
+					chatlog.Std.Info("字段匹配成功")
+
+					break
+				}
+			}
+			fieldobj := confV.Elem().FieldByName(fieldName)
+			switch field.Type.Kind() {
+			case reflect.String:
+				fieldobj.SetString(value)
+			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32:
+				v, err := strconv.ParseInt(value, 10, 64)
+				if err != nil {
+					chatlog.Std.Errorf("%v 转换int64失败", value)
+					return
+				}
+				fieldobj.SetInt(v)
 			}
 		}
 	}
